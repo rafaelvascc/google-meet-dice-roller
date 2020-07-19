f863663d7b1b50e4c59163803af85b36629bb9e9
Rafael Vasconcelos <rafaelvascc@gmail.com>
2020-07-19 12:30:26 -0300

    Some refactoring and bug fixes

---
+++ src/functions/formatting.js
@@ -0,0 +1,22 @@
+const formatRoll = (roll) => {
+    const {
+        rolls,
+        successes,
+        failures,
+        success,
+        sum,
+        difficulty,
+        limit,
+        target,
+        negate,
+        plus, 
+        minus,
+        command
+    } = roll;
+
+    return `${command}:\n\t (${rolls.join(', ')})${plus ? ` +${plus} ` : minus ? ` -${minus} ` :  ''}${successes !== null ? ` Successes: ${successes}` : ''} Total: ${sum}${target ? ` >= ${target},` : negate ? ` <= ${negate},` : ''}${success !== null ? success ? ` SUCCESS ` : ` FAILURE ` : ''}\n`;
+}
+
+const formatTotalResult = (total, constant) => {
+    return `Total: ${constant !== 0 ? (constant > 0 ? `${total} + ${constant} = ${total + constant}` : `${total} - ${constant * -1} = ${total + constant}` ): total}\n`;
+}
