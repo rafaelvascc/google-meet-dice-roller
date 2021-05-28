fa71570d1f9c0abea27b202fba59ebec84cfe411
Rafael Vasconcelos <rafaelvascc@gmail.com>
2021-05-28 18:53:26 -0300

    small fix for regular expression

--- src/constants/regular-expressions.js
+++ src/constants/regular-expressions.js
@@ -1,4 +1,4 @@
 export const diceRegex = /^([+-]*)(\d*)d(\d+)(([hl])(\d+))?(([dm])(\d+))?(([\*+-])(\d+))?(([tn])(\d+))?$/;
 export const constRegex = /^([+-])(\d+)$/;
 export const commandRegex = /^((([+-]*)(\d*)d(\d+)(([hl])(\d+))?(([dm])(\d+))?(([\*+-])(\d+))?(([tn])(\d+))?)|\s|(([+-])(\d+)))+$/;
-export const variableRegex = /^\-?((Math\.(ceil|floor|round)\(|\())?(\-?\(?(\d+|(\{([^.\s\/\\\{\}\(\)]+?)\})))\)?([\+\-\*\/]((Math\.(ceil|floor|round)\(|\())?(\d+|(\{([^.\s\/\\\{\}\(\)]+?)\}))\)?)*\)?$/;
+export const variableRegex = /^\-?((Math\.(ceil|floor|round)\(*|\(*))?(\-?\(?(\d+|(\{([^.\s\/\\\{\}\(\)]+?)\})))\)*([\+\-\*\/]((Math\.(ceil|floor|round)\(*|\(*))?(\d+|(\{([^.\s\/\\\{\}\(\)]+?)\}))\)*)*\)?$/;
