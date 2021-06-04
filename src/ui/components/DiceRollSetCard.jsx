d0c9934b6a24a25f1d9b7b24ef6554cb64145087
Rafael Vasconcelos <rafaelvascc@gmail.com>
2021-06-04 00:28:54 -0300

    replacing react bootstrap tabs component with simple selector using buttons (tabs component was reloading google meet page)

--- src/ui/components/DiceRollSetCard.jsx
+++ src/ui/components/DiceRollSetCard.jsx
@@ -3,6 +3,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ButtonWithTolltip from './ButtonWithTolltip.jsx';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import UserDiceRollItemForm from './UserDiceRollItemForm.jsx';
@@ -25,6 +26,7 @@ const DiceRollSetCard = (props) => {
     const [addVariablePopoverVisible, setAddVariablePopoverVisible] = useState(false);
     const [addDiceRollPopoverVisible, setAddDiceRollPopoverVisible] = useState(false);
     const [removeSetPopoverVisible, setRemoveSetPopoverVisible] = useState(false);
+    const [selectedTab, setSelectedTab] = useState('cmds')
 
     const addVariablePopoverBtnRef = useRef(null);
     const addDiceRollPopoverBtnRef = useRef(null);
@@ -52,6 +54,51 @@ const DiceRollSetCard = (props) => {
         setAddVariablePopoverVisible(false);
         setAddDiceRollPopoverVisible(false);
         setRemoveSetPopoverVisible(false);
+    }
+
+    const getTab = (tabName) => {
+        if (selectedTab === "cmds") {
+            return (
+                <>
+                    <Form.Row style={{ marginBottom: "0px", marginTop: "16px" }}>
+                        <Form.Group as={Col} sm={4} controlId="commandLabelHeader" style={{ marginBottom: "0px" }}>
+                            <Form.Label>Label</Form.Label>
+                        </Form.Group>
+                        <Form.Group as={Col} sm={6} controlId="commandHeader" style={{ marginBottom: "0px" }}>
+                            <Form.Label>Command</Form.Label>
+                        </Form.Group>
+                    </Form.Row>
+                    {
+                        Object.keys(props.set.commands).map((k) => {
+                            return (
+                                <UserDiceRollItemForm key={k} setName={props.setName} set={props.set} label={k} command={props.set.commands[k]} />
+                            )
+                        })
+                    }
+                </>
+            );
+        }
+        if (selectedTab === "vars") {
+            return (
+                <>
+                    <Form.Row style={{ marginBottom: "0px", marginTop: "16px" }}>
+                        <Form.Group as={Col} sm={4} controlId="variableLabelHeader" style={{ marginBottom: "0px" }}>
+                            <Form.Label>Label</Form.Label>
+                        </Form.Group>
+                        <Form.Group as={Col} sm={6} controlId="variableHeader" style={{ marginBottom: "0px" }}>
+                            <Form.Label>Variable/Expression</Form.Label>
+                        </Form.Group>
+                    </Form.Row>
+                    {
+                        Object.keys(props.set.variables).map((k) => {
+                            return (
+                                <UserVariableForm key={k} setName={props.setName} set={props.set} label={k} expression={props.set.variables[k]} />
+                            )
+                        })
+                    }
+                </>
+            );
+        }
     }
 
     return (
@@ -82,7 +129,7 @@ const DiceRollSetCard = (props) => {
                                         y="12.50646"
                                         x="0.18556"
                                         fill="#ffffff">
-                                            (X)
+                                        (X)
                                         </text>
                                 </g>
                             </svg>
@@ -127,42 +174,11 @@ const DiceRollSetCard = (props) => {
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={props.setName}>
                 <Card.Body style={{ paddingTop: "8px", paddingBottom: "8px", paddingRight: "16px", paddingLeft: "16px" }}>
-                    <Tabs defaultActiveKey="commands" id={`tabs-${props.setName}`} style={{ marginLeft: "auto" }} className="nav-pills">
-                        <Tab eventKey="commands" title="Commands">
-                            <Form.Row style={{ marginBottom: "0px", marginTop: "16px" }}>
-                                <Form.Group as={Col} sm={4} controlId="commandLabelHeader" style={{ marginBottom: "0px" }}>
-                                    <Form.Label>Label</Form.Label>
-                                </Form.Group>
-                                <Form.Group as={Col} sm={6} controlId="commandHeader" style={{ marginBottom: "0px" }}>
-                                    <Form.Label>Command</Form.Label>
-                                </Form.Group>
-                            </Form.Row>
-                            {
-                                Object.keys(props.set.commands).map((k) => {
-                                    return (
-                                        <UserDiceRollItemForm key={k} setName={props.setName} set={props.set} label={k} command={props.set.commands[k]} />
-                                    )
-                                })
-                            }
-                        </Tab>
-                        <Tab eventKey="variables" title="Variables">
-                            <Form.Row style={{ marginBottom: "0px", marginTop: "16px" }}>
-                                <Form.Group as={Col} sm={4} controlId="variableLabelHeader" style={{ marginBottom: "0px" }}>
-                                    <Form.Label>Label</Form.Label>
-                                </Form.Group>
-                                <Form.Group as={Col} sm={6} controlId="variableHeader" style={{ marginBottom: "0px" }}>
-                                    <Form.Label>Variable/Expression</Form.Label>
-                                </Form.Group>
-                            </Form.Row>
-                            {
-                                Object.keys(props.set.variables).map((k) => {
-                                    return (
-                                        <UserVariableForm key={k} setName={props.setName} set={props.set} label={k} expression={props.set.variables[k]} />
-                                    )
-                                })
-                            }
-                        </Tab>
-                    </Tabs>
+                    <Form.Row>
+                        <Button variant="success" size="sm" onClick={() => setSelectedTab('cmds')} style={selectedTab === "cmds" ? { boxShadow: "0px 0px 0px 4px palegreen", marginRight: "8px" } : { marginRight: "8px" }}>Commands</Button>
+                        <Button variant="primary" size="sm" onClick={() => setSelectedTab('vars')} style={selectedTab === "vars" ? { boxShadow: "0px 0px 0px 4px cyan" } : {}}>Variables</Button>
+                    </Form.Row>
+                    {getTab(selectedTab)}
                 </Card.Body>
             </Accordion.Collapse>
         </Card>
